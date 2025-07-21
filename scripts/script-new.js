20742cc30b251f72197791677599e47134aaf3c5
Diego04012002 <dgarciadeus@gmail.com>
2025-07-21 18:47:46 +0200

    feat: update dependency installation and refactor data insertion method for Supabase

--- scripts/script-new.js
+++ scripts/script-new.js
@@ -1,13 +1,19 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_URL_SUPABASE,
-  process.env.NEXT_PUBLIC_API_KEY_SUPABASE
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_URL_SUPABASE
+const supabaseKey = process.env.NEXT_PUBLIC_API_KEY_SUPABASE
 
 async function insert(){
-  await supabase.from("test").insert({
-    content:"Prueba de cron 1"
+  const content={
+    "content" : "Prueaba desde script"
+  }
+  
+  await fetch(`${supabaseUrl}/rest/v1/curiosities`, {
+    method: "POST",
+    headers: {
+      "apikey": supabaseKey,
+      "Content-Type": "application/json",
+      "Prefer": "resolution=merge-duplicates"
+    },
+    body: JSON.stringify(content)
   })
 }
 
